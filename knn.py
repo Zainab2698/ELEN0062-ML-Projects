bbb35e8b958eef24a6cc66143188531bcdd609f7
theofgt <theo.faingnaert@gmail.com>
2024-10-24 16:37:54 +0200

    Q4

--- knn.py
+++ knn.py
@@ -8,6 +8,7 @@ Project 1 - Classification algorithms
 
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.model_selection import cross_val_score
 
 from data import make_dataset
 from plot import plot_boundary
@@ -39,8 +40,36 @@ def Q2():
     average = accuracies.mean(axis=0)
     std_deviations = accuracies.std(axis=0)
     print("average accuracy =", average) 
-    print("standard deviation =", std_deviations)    
+    print("standard deviation =", std_deviations)
+
+def Q4():
+    n = 1
+    max_n = 0
+    max_score = 0
+    X,y = make_dataset(3000, random_state=100)
+    while n < 500:
+        clf =  KNeighborsClassifier(n_neighbors=n)
+        score = cross_val_score(clf, X[:1000], y[:1000], cv=5).mean()
+        if(score > max_score):
+            max_score = score
+            max_n = n
+        n += 1
+
+    accuracies = np.empty((5,))
+    seeds = [97, 82, 42, 16, 76]
+    for generation in range(5):
+        X,y = make_dataset(3000, random_state=seeds[generation])
+        clf =  KNeighborsClassifier(n_neighbors=max_n)
+        fit = clf.fit(X[:1000], y[:1000])
+        y_prediction = fit.predict(X[1000:])
+        accuracies[generation] = accuracy_score(y[1000:], y_prediction)
+    average = accuracies.mean()
+    std = accuracies.std()
+    print("\nTuned parameter =", max_n)
+    print("average accuracy =", average) 
+    print("standard deviation =", std)
 
 if __name__ == "__main__":
     Q1()
     Q2()
+    Q4()
